589ddde866d2d0349ddac94c9d357afc803e16cc
mantvmass <kliop2317@gmail.com>
2023-11-18 18:09:48 +0700

    update 'update message'

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -2,12 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 
 
 // load views
-import Home from "../views/Home";
+// import Home from "../views/Home";
 import NotFound from "../views/error/NotFound";
 
 
 const routes = createBrowserRouter([
-    { path: "/mantvmass/", element: <Home/> },
+    { path: "/mantvmass/", element: <h4>อยู่รหว่างการอัพเดท...</h4> },
+    // { path: "/mantvmass/", element: <Home/> },
     { path: "/mantvmass/*", element: <NotFound/> }
 ])
 
